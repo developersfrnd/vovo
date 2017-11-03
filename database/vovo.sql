7ff4080eee6b73b0248639b02e5b7cdca6e71d56
prashant-singh-kiwitech <prashant.singh.kiwitech@gmail.com>
2017-11-03 19:06:36 +0530

    database

---
+++ database/vovo.sql
@@ -0,0 +1,268 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 03, 2017 at 07:05 PM
+-- Server version: 5.5.55-0ubuntu0.14.04.1
+-- PHP Version: 5.6.31-6+ubuntu14.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `vovo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banks`
+--
+
+CREATE TABLE IF NOT EXISTS `banks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `bank_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `account_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `neft_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tax_id` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bank_address` text COLLATE utf8_unicode_ci,
+  `paypal_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'for international users',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `banks`
+--
+
+INSERT INTO `banks` (`id`, `user_id`, `bank_name`, `account_number`, `neft_code`, `tax_id`, `bank_address`, `paypal_email`, `created_at`, `updated_at`) VALUES
+(1, 5, 'dena bank', '876876876876', 'sdf', 'sdf', 'Noida', NULL, '2017-10-26 15:44:19', '2017-10-26 15:44:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` text COLLATE utf8_unicode_ci,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `display` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `depth` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `parent_id`, `meta_keyword`, `meta_description`, `name`, `slug`, `display`, `description`, `image`, `depth`, `status`, `created_at`, `updated_at`) VALUES
+(4, 0, 'Meta tag keyword', 'meta tag description', 'name', 'name', 'name', NULL, 'JVfkqnOZp524m0QLphpfIYSPbHJ9wDlE5rJmGca8.jpeg', 0, 1, '2017-10-25 11:45:36', '2017-10-25 11:45:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE IF NOT EXISTS `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tax_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `store_url` text COLLATE utf8_unicode_ci,
+  `about_company` text COLLATE utf8_unicode_ci,
+  `address1` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `city` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `state` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country_id` int(11) NOT NULL,
+  `zip` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `user_id`, `company_id`, `name`, `tax_id`, `store_url`, `about_company`, `address1`, `address2`, `city`, `state`, `country_id`, `zip`, `created_at`, `updated_at`) VALUES
+(1, 4, 12345, 'My company', 'sdf', 'www.google.com', 'Its mine', 'Noida@address1.com', 'U.P.', 'Delhi', 'Delhi', 91, '221001', '2017-10-26 15:41:35', '2017-10-26 15:41:35'),
+(2, 5, 12345, 'My company', 'sdf', 'www.google.com', 'Its mine', 'Noida@address1.com', 'U.P.', 'Delhi', 'Delhi', 91, '221001', '2017-10-26 15:44:19', '2017-10-26 15:44:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `main_image` tinyint(4) NOT NULL COMMENT '1=>yes',
+  `display_order` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `product_id`, `image`, `main_image`, `display_order`, `created_at`, `updated_at`) VALUES
+(1, 5, 'products/5/CCbNB3nr3Np5YCoS2vqr7K9CAW6Ui7G2pIKwJA7J.jpeg', 1, 0, '2017-11-03 07:43:05', '2017-11-03 07:43:05'),
+(2, 5, 'products/5/D8mDEETHUWwE27NwAdGHkPafhBaWuM0PwImtJZ1r.png', 0, 1, '2017-11-03 07:43:05', '2017-11-03 07:43:05'),
+(3, 5, 'products/5/i3cfb2cDpZUxHxg0baeM3LN20fU4CwEw73QV8iCM.png', 0, 2, '2017-11-03 07:43:05', '2017-11-03 07:43:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prices`
+--
+
+CREATE TABLE IF NOT EXISTS `prices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `special_price` decimal(10,0) DEFAULT NULL,
+  `discount` decimal(10,0) DEFAULT NULL,
+  `discount_percent_number` tinyint(4) NOT NULL COMMENT '0=>percent,1=>number',
+  `from_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `to_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `meta_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` text COLLATE utf8_unicode_ci,
+  `short description` text COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
+  `sku` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `weight` decimal(10,0) DEFAULT NULL,
+  `new_from_date` timestamp NULL DEFAULT NULL,
+  `new_to_date` timestamp NULL DEFAULT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_date` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `created_date`, `updated_at`) VALUES
+(1, 'customer', NULL, NULL),
+(2, 'vendor', NULL, NULL),
+(3, 'admin', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_users`
+--
+
+CREATE TABLE IF NOT EXISTS `role_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fax` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `first_name`, `last_name`, `email`, `password`, `remember_token`, `phone`, `fax`, `status`, `created_at`, `updated_at`) VALUES
+(1, 0, 'admin', NULL, 'admin@yopmail.com', '$2y$10$GQi7eJf8Khetr.x6Jt6VMeJnKyifTFNUQ6KjVlBHMx.1VJHxCivvK', 'dEn7SgI5ZtdAqif1OIcu98CHsd8lYVKnHuCxkKfA5nhLVNlXAnTNDsP4wyhR', NULL, NULL, 1, '2017-10-08 19:28:04', '2017-10-08 19:28:04'),
+(5, 2, 'Vendor', 'new', 'vendor@yopmail.com', '$2y$10$21KDFBOrtysPBO6byVaBTO4oM468MI0lMExLSqmk2o33FWfOCuJ7.', 'MXe1qz1mevsBt7bdpIQu1QmPRU6AyJvrtJbqvkNAT5JX26sYJ30l7P1cqjhs', NULL, NULL, 1, '2017-10-26 15:44:19', '2017-10-26 15:44:19');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
